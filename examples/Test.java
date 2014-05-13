67e9bf65deefd282fea849892d9ef5339dbb7298
ruoshui1126 <ruoshui1126@gmail.com>
2014-05-13 20:05:49 +0800

    examples

--- examples/Test.java
+++ examples/Test.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-import edu.hit.ir.ltpNative.*;
+import edu.hit.ir.ltp4j.*;
 
 
 public class Test {
   public static void init(){
-    SegmentJNI.segmentorCreate("../../../ltp_data/cws.model");
-    PostagJNI.postaggerCreate("../../../ltp_data/pos.model");
-    NerJNI.nerCreate("../../../ltp_data/ner.model");
-    ParserJNI.parserCreate("../../../ltp_data/parser.model");
-    SrlJNI.srlCreate("../../../ltp_data/srl");
+    Segment.create("../ltp_data/cws.model");
+    Postag.create("../ltp_data/pos.model");
+    Ner.create("../ltp_data/ner.model");
+    Parser.create("../ltp_data/parser.model");
+    Srl.create("../ltp_data/srl");
   }
 
   public static void ltp(String sent){
     ArrayList<String> sents = new ArrayList<String>(); 
-    SplitSentenceJNI.splitSentence(sent,sents);
+    SplitSentence.splitSentence(sent,sents);
     System.out.println(sent);
     System.out.println("sents:"+sents.size());
     for(int m = 0;m<sents.size();m++){
@@ -28,7 +28,7 @@ public class Test {
       List<Pair<Integer, List<Pair<String, Pair<Integer, Integer>>>>> srls = 
         new ArrayList<Pair<Integer, List<Pair<String, Pair<Integer, Integer>>>>>();
 
-      SegmentJNI.segmentorSegment(sents.get(m),words);
+      Segment.segment(sents.get(m),words);
       int size = words.size();
       System.out.println("words:"+size);
       for(int i = 0;i<size;i++) {
@@ -41,7 +41,7 @@ public class Test {
         }
       }
 
-      PostagJNI.postaggerPostag(words,postags);
+      Postag.postag(words,postags);
       size = postags.size();
       System.out.println("postags:"+size);
       for(int i = 0;i<size;i++) {
@@ -55,7 +55,7 @@ public class Test {
       }
 
 
-      NerJNI.nerRecognize(words,postags,ners);
+      Ner.recognize(words,postags,ners);
       size = postags.size();
       System.out.println("ners:"+size);
       for(int i = 0;i<size;i++) {
@@ -68,7 +68,7 @@ public class Test {
         }
       }
 
-      ParserJNI.parserParse(words,postags,heads,deprels);
+      Parser.parse(words,postags,heads,deprels);
       size = heads.size();
       System.out.println("parser:"+size);
       for(int i = 0;i<size;i++) {
@@ -85,7 +85,7 @@ public class Test {
         heads.set(i,heads.get(i)-1);
       }
 
-      SrlJNI.srlSrl(words,postags,ners,heads,deprels,srls);
+      Srl.srl(words,postags,ners,heads,deprels,srls);
       size = srls.size();
       System.out.println("srl:"+size);
       for (int i = 0; i < srls.size(); i++) {
@@ -99,24 +99,27 @@ public class Test {
     }
   }
   public static void release(){
-    SegmentJNI.segmentorRelease();
-    PostagJNI.postaggerRelease();
-    NerJNI.nerRelease();
-    ParserJNI.parserRelease();
-    SrlJNI.srlRelease();
+    Segment.release();
+    Postag.release();
+    Ner.release();
+    Parser.release();
+    Srl.release();
   }
 
 public static void main(String[] args) {
   init();
   Scanner input = new Scanner(System.in);
   String sent;
-  while((sent = input.nextLine())!=null){
-    if(sent.length()>0){
-      ltp(sent);
+  try {
+    while((sent = input.nextLine())!=null){
+      if(sent.length()>0){
+        ltp(sent);
+      }
     }
-    ltp(sent);
   }
-  release();
+  catch(Exception e) {
+    release();
+  }
 }
 
 }
